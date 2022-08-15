6488eefee7d1f9b4ad8e2dd67b2ccc658d39f0e2
DOMINGO CARVAJAL <domingo.carvajal@uc.cl>
2022-08-14 20:16:59 -0400

    (feat) : code strcture

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+
+json_file_path = "data2.json"
+
+with open(json_file_path, 'r') as j:
+     contents = json.loads(j.read())
+     data = contents["data"]
+
+def retweeted():
+    pass
+
+def users():
+    pass
+
+def days():
+    pass
+
+def hashtags():
+    pass
+
+def main():
+    pass
